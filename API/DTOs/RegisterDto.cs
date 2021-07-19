329cedd32175ca924e436ba64c45c4c6c1e66837
cuong0101 <phuongcuong1504@gaml.com>
2021-07-19 15:48:42 +0700

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,13 +1,26 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
-        public string Username { get; set; }
+        public RegisterDto(string username, string knownAs, string gender, DateTime dateOfBirth, string password) 
+        {
+            this.Username = username;
+                this.KnownAs = knownAs;
+                this.Gender = gender;
+                this.DateOfBirth = dateOfBirth;
+                this.Password = password;
+               
+        }
+                [Required]  public string Username { get; set; }
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required]public DateTime DateOfBirth { get; set; }
         [Required]
         [StringLength(8,MinimumLength = 4)]
         public string Password { get; set; }
     }
+
 }
